adcfc149d7fe993bd3ef253bf47bc68552cc48cb
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-11-10 03:36:34 +0800

    Login Menu Update
    Bootstrap style has been applied and the entire thing is now on the center of the page.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 function Login() {
     return (
-        <div>
+        <div className = "d-flex justify-content-center">
 		<Head>
                 <title>DPSM-QA-PORTAL</title>
                 {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link> */}
@@ -12,15 +12,26 @@ function Login() {
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
             </Head>
+	<div className = "jumbotron">
             <form action="">
                 <label htmlFor="email">UP Email:</label>
-                <input className = "form-control col-md-3 float-center" type="text" id="email" name="email" />
+                <input className = "form-control" type="text" id="email" name="email" />
                 
                 <label htmlFor="password">Password:</label>
-                <input className = "form-control col-md-3 float-center" type="text" id="password" name="password" />
+                <input className = "form-control" type="text" id="password" name="password" />
 		<br />
                 <Link href="/faculty"><input type="submit" className = "btn btn-primary" value="Login"></input></Link>
             </form>
+	</div>
+	<style jsx>{`
+		.d-flex{
+			min-height: 100%;
+  			min-height: 100vh; 
+
+  			display: flex;
+  			align-items: center;
+		}
+	`}</style>
         </div>
     )
   }
