ef0ec0a8fd31ee94b6072d1e82e7a5b16e70d832
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-11-11 02:14:00 +0800

    Accordion menu implemented
    The Faculty menu button should now function as desired

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,17 +2,15 @@ import Link from 'next/link'
 
 function Sidebar() {
     return (
-            <div className="col-1">
-                <div className="dropdown">
-		    <button className = "btn btn-success btn-lg dropdown-togggle" type = "button" id = "facultyDropdown" data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "false">
-			Faculty
-		    </button>
-			<div className = "dropdown-menu" aria-labelledby = "facultyDropdown">
-                    <Link href="/faculty"><a className = "dropdown-item">Faculty</a></Link>
-                    <Link href="/faculty/basic-info"><a className = "dropdown-item">Basic Information</a></Link>
-                    <Link href="/faculty/accomplishment"><a className = "dropdown-item">Accomplishment</a></Link>
-                    <Link href="/faculty/class"><a className = "dropdown-item">Class</a></Link>
-                    <Link href="/faculty/evaluation"><a className = "dropdown-item">Evaluation</a></Link>
+            <div className="col-2">
+                <div className="list-group">
+			<a className = "list-group-item list-group-item-action list-group-item-secondary" data-toggle = "collapse" href = "#faculty"> Faculty </a>
+			<div id = "faculty" className = "collapse">
+                    <Link href="/faculty"><a className = "list-group-item list-group-item-action list-group-item-success">Dashboard</a></Link>
+                    <Link href="/faculty/basic-info"><a className = "list-group-item list-group-item-action list-group-item-success">Basic Information</a></Link>
+                    <Link href="/faculty/accomplishment"><a className = "list-group-item list-group-item-action list-group-item-success">Accomplishment</a></Link>
+                    <Link href="/faculty/class"><a className = "list-group-item list-group-item-action list-group-item-success">Class</a></Link>
+                    <Link href="/faculty/evaluation"><a className = "list-group-item list-group-item-action list-group-item-success">Evaluation</a></Link>
 			</div>
                 </div>
                 <div className="list-group">
@@ -26,7 +24,7 @@ function Sidebar() {
                 </div>
 
 		<style jsx>{`
-			.col-1 {
+			.col-2 {
   				background-color: #017823; 
 			}
 		`}</style>
