05adf332a5bac3ad78e04a784e01e0e00eac9c2c
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-11-11 02:21:02 +0800

    Changed data fields
    Extraneous fields have been removed.

--- components/faculty/basic-info/work-experience.js
+++ components/faculty/basic-info/work-experience.js
@@ -7,10 +7,7 @@ function WorkExperience(){
 			<tbody>
 				<tr>
 					<th>Employer</th>
-					<th>Address</th>
 					<th>Position</th>
-					<th>Contact Number</th>
-					<th>Supervisor</th>
 					<th>Start Date</th>
 					<th>End Date</th>
 					<th>Description</th>
@@ -21,9 +18,6 @@ function WorkExperience(){
 					<td>2020-01-26</td>
 					<td></td>
 					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
 				</tr>
 				<tr>
 					<td>Professor 1</td>
@@ -31,18 +25,12 @@ function WorkExperience(){
 					<td>2017-05-26</td>
 					<td>2020-01-25</td>
 					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
 				</tr>
 				<tr>
 					<td>Instructor 1</td>
 					<td>University of the Philippines Los Banos</td>
 					<td>2010-09-06</td>
 					<td>2017-05-25</td>
-					<td></td>
-					<td></td>
-					<td></td>
 					<td></td>
 				</tr>
 			</tbody>
@@ -60,40 +48,28 @@ function WorkExperience(){
 		</div>
 		<br />
 		<div className = "form-row">
-			<div className = "form-group col-md-2">
+			<div className = "form-group col-md-3">
 				<label htmlFor = "EmployerWorkExperience[]"> Employer </label>
 				<input className = "form-control" type = "text" name = "EmployerWorkExperience[]" placeholder = "Input name of employer" />
 			</div>
-			<div className = "form-group col-md-4">
-				<label htmlFor = "AddressWorkExperience[]"> Address </label>
-				<input className = "form-control" type = "text" name = "AddressWorkExperience[]" placeholder = "Input address of employer" />
-			</div>
-			<div className = "form-group col-md-2">
+			<div className = "form-group col-md-3">
 				<label htmlFor = "PositionWorkExperience[]"> Title/Position </label>
 				<input className = "form-control" type = "text" name = "PositionWorkExperience[]" placeholder = "Input position" />
 			</div>
-			<div className = "form-group col-md-2">
-				<label htmlFor = "ContactNumberWorkExperience[]"> Contact Number </label>
-				<input className = "form-control" type = "text" name = "ContactNumberWorkExperience[]" placeholder = "Input contact number" />
-			</div>
-			<div className = "form-group col-md-2">
-				<label htmlFor = "SupervisorWorkExperience[]"> Supervisor </label>
-				<input className = "form-control" type = "text" name = "SupervisorWorkExperience[]" placeholder = "Input name of supervisor" />
-			</div>
 		</div>
 		<div className = "form-row">
-			<div className = "form-group col-md-2">
+			<div className = "form-group col-md-3">
 					<label htmlFor = "StartDateWorkExperience[]"> Start Date </label>
 					<input type = "date" className = "form-control" name = "StartDateWorkExperience[]" />
 				</div>
-				<div className = "form-group col-md-2">
+				<div className = "form-group col-md-3">
 					<label htmlFor = "EndDateWorkExperience[]"> End Date </label>
 					<input type = "date" className = "form-control" name = "EndDateWorkExperience[]" />
 			</div>
 		</div>
 		<div className = "form-group">
 			<label htmlFor = "DescriptionWorkExperience[]"> Description </label>
-			<input className = "form-control" type = "textarea" name = "DescriptionWorkExperience[]" placeholder = "Add Description" />
+			<input className = "form-control col-md-6" type = "text" name = "DescriptionWorkExperience[]" placeholder = "Add Description" />
 		</div>
 		<div id = "WorkExperience">
 		</div>
