5cd9e15bf19ae57f94d6cd888de9361d90cea2f6
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-11-21 09:54:20 +0800

    Added new fields
    Form duplication function also restored.

--- components/faculty/accomplishments/licensure-exam-form.js
+++ components/faculty/accomplishments/licensure-exam-form.js
@@ -9,7 +9,7 @@ class LicensureExamForm extends React.Component{
 	}
 	clone(){
 		this.state.duplicateForms.push(<div><hr /><div className = "form-row">
-                        <div className = "form-group col-md-2">
+			<div className = "form-group col-md-2">
                             <label htmlFor = "LicensureExam[]"> Licensure Exam </label>
                             <input className = "form-control" type = "text" name = "LicensureExam[]" placeholder = "Input licensure exam" />
                         </div>
@@ -20,6 +20,14 @@ class LicensureExamForm extends React.Component{
 			<div className = "form-group col-md-2">
                             <label htmlFor = "LicensureExamRank[]"> Rank </label>
                             <input className = "form-control" type = "text" name = "LicensureExamRank[]" placeholder = "Input rank" />
+                        </div>
+			<div className = "form-group col-md-2">
+                            <label htmlFor = "LicenseNumber[]"> License Number </label>
+                            <input className = "form-control" type = "text" name = "LicenseNumber[]" placeholder = "Input licensure number" />
+                        </div>
+			<div className = "form-group col-md-2">
+                            <label htmlFor = "LicensureExamProof[]"> Proof </label>
+                            <input type = "file" className = "form-control-file" name = "LicensureExamProof[]" />
                         </div>
                     </div>
 			<style jsx>{`
@@ -42,7 +50,7 @@ class LicensureExamForm extends React.Component{
 		return(
 			<form>
                     <hr />
-                   {/* <div className = "form-row">
+                   <div className = "form-row">
                         <div className = "col-auto">
                             <button type = "button" className = "btn btn-primary" id = "AddAccomplishment" onClick = {() => this.clone()}> Add Licensure Exam </button>
                         </div>
@@ -50,8 +58,12 @@ class LicensureExamForm extends React.Component{
                             <button type = "button" className = "btn btn-danger" id = "RemoveAccomplishment" onClick = {() => this.remove()}> Remove a Row </button>
                         </div>
                     </div>
-                    <br /> */}
+                    <br />
                     <div className = "form-row">
+			<div className = "form-group col-md-2">
+                            <label htmlFor = "LicensureExam[]"> Licensure Exam </label>
+                            <input className = "form-control" type = "text" name = "LicensureExam[]" placeholder = "Input licensure exam" />
+                        </div>
 			<div className = "form-group col-md-2">
                             <label htmlFor = "LicensureExamDate[]"> Date </label>
                             <input type = "date" className = "form-control" name = "LicensureExamDate[]" />
@@ -59,6 +71,10 @@ class LicensureExamForm extends React.Component{
 			<div className = "form-group col-md-2">
                             <label htmlFor = "LicensureExamRank[]"> Rank </label>
                             <input className = "form-control" type = "text" name = "LicensureExamRank[]" placeholder = "Input rank" />
+                        </div>
+			<div className = "form-group col-md-2">
+                            <label htmlFor = "LicenseNumber[]"> License Number </label>
+                            <input className = "form-control" type = "text" name = "LicenseNumber[]" placeholder = "Input licensure number" />
                         </div>
 			<div className = "form-group col-md-2">
                             <label htmlFor = "LicensureExamProof[]"> Proof </label>
