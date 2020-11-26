2a103cd11e414c6ec96e3758abdede26d81f3357
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-11-26 20:23:42 +0800

    Added new table
    There are now two tables, one for accomplishments within UPM, and accomplishments outside UPM.

--- components/faculty/accomplishments/public-service-accomplishment.js
+++ components/faculty/accomplishments/public-service-accomplishment.js
@@ -4,27 +4,62 @@ import PublicServiceAccomplishmentForm from './public-service-accomplishment-for
 function PublicServiceAccomplishment(){
 	return(
 	<div>
-	
-            <div>
+		<div>
+		<h5 align = "center">Within UP Manila </h5>
                 <table className = "table table-striped table-sm">
                     <tbody>
                         <tr>
-                            <th>Accomplishment</th>
+                            <th>Position/Role</th>
+			    <th>Organization</th>
 			    <th>Description</th>
                             <th>Date</th>
                             <th>Proof</th>
                             <th>Status</th>
                         </tr>
                         <tr>
-                            <td>Failed COVID-19 response</td>
-                            <td>Not implementing mass testing or contact tracing</td>
+			    <td></td>
+                            <td></td>
+                            <td></td>
 			    <td>2020-02-29</td>
                             <td></td>
                             <td><a className="btn btn-info" data-toggle="modal" data-target="#ongoingstatus">Ongoing</a></td>
                         </tr>
                         <tr>
-                            <td>Corruption</td>
-                            <td>Embezzlement of taxpayer funds</td>
+			    <td></td>
+                            <td></td>
+                            <td></td>
+			    <td>2020-03-20</td>
+                            <td></td>
+                            <td><a className="btn btn-info" data-toggle="modal" data-target="#forVerifstatus">For Verification</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+		<br />
+		<h5 align = "center">Outside UP Manila </h5>
+                <table className = "table table-striped table-sm">
+                    <tbody>
+                        <tr>
+                            <th>Position/Role</th>
+			    <th>Organization</th>
+			    <th>Description</th>
+                            <th>Date</th>
+                            <th>Proof</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+			    <td></td>
+                            <td></td>
+                            <td></td>
+			    <td>2020-02-29</td>
+                            <td></td>
+                            <td><a className="btn btn-info" data-toggle="modal" data-target="#ongoingstatus">Ongoing</a></td>
+                        </tr>
+                        <tr>
+			    <td></td>
+                            <td></td>
+                            <td></td>
 			    <td>2020-03-20</td>
                             <td></td>
                             <td><a className="btn btn-info" data-toggle="modal" data-target="#forVerifstatus">For Verification</a></td>
