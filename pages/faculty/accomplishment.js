e18c1f011bc77cc8b90b9874c95f95658e30f7f9
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-12-01 13:34:02 +0800

    Removed Academic Accomplishments

--- pages/faculty/accomplishment.js
+++ pages/faculty/accomplishment.js
@@ -1,6 +1,5 @@
 import Layout from '../../components/layout'
 import Publication from '../../components/faculty/accomplishments/publication'
-import AcademicAccomplishment from '../../components/faculty/accomplishments/academic-accomplishment'
 import PublicServiceAccomplishment from '../../components/faculty/accomplishments/public-service-accomplishment'
 import LicensureExam from '../../components/faculty/accomplishments/licensure-exam'
 import TrainingSeminar from '../../components/faculty/accomplishments/training-seminar'
@@ -11,8 +10,8 @@ function Dashboard() {
         <Layout>
             <nav>
             <div className="nav nav-tabs nav-fill nav-justified" id="nav-tab" role="tablist">
-                <a className="nav-item nav-link active" id="academic-accomplishment-tab" data-toggle="tab" href="#academic-accomplishment" role="tab" aria-controls="academic-accomplishment" aria-selected="true">Academic Accomplishments</a>
-		<a className="nav-item nav-link" id="public-service-accomplishment-tab" data-toggle="tab" href="#public-service-accomplishment" role="tab" aria-controls="public-service-accomplishment" aria-selected="false">Public Service Accomplishments</a>
+                {/* <a className="nav-item nav-link active" id="academic-accomplishment-tab" data-toggle="tab" href="#academic-accomplishment" role="tab" aria-controls="academic-accomplishment" aria-selected="true">Academic Accomplishments</a> */}
+		<a className="nav-item nav-link active" id="public-service-accomplishment-tab" data-toggle="tab" href="#public-service-accomplishment" role="tab" aria-controls="public-service-accomplishment" aria-selected="true">Public Service Accomplishments</a>
 		<a className="nav-item nav-link" id="publication-tab" data-toggle="tab" href="#publication" role="tab" aria-controls="publication" aria-selected="false">Publications</a>
 		<a className="nav-item nav-link" id="training-seminar-tab" data-toggle="tab" href="#training-seminar" role="tab" aria-controls="training-seminar" aria-selected="false">Training/Seminars</a>
 		<a className="nav-item nav-link" id="licensure-exam-tab" data-toggle="tab" href="#licensure-exam" role="tab" aria-controls="licensure-exam" aria-selected="false">Licensure Exams</a>
@@ -22,8 +21,8 @@ function Dashboard() {
 		<br />
 		<br />
             <div className="tab-content" id="nav-tabContent">
-            <div className="tab-pane fade show active" id="academic-accomplishment" role="tabpanel" aria-labelledby="academic-accomplishment-tab"><AcademicAccomplishment /></div>
-	    <div className="tab-pane fade" id="public-service-accomplishment" role="tabpanel" aria-labelledby="public-service-accomplishment-tab"><PublicServiceAccomplishment /></div>
+            {/* <div className="tab-pane fade show active" id="academic-accomplishment" role="tabpanel" aria-labelledby="academic-accomplishment-tab"><AcademicAccomplishment /></div> */}
+	    <div className="tab-pane fade show active" id="public-service-accomplishment" role="tabpanel" aria-labelledby="public-service-accomplishment-tab"><PublicServiceAccomplishment /></div>
 	    <div className="tab-pane fade" id="publication" role="tabpanel" aria-labelledby="publication-tab"><Publication /></div>
 	    <div className="tab-pane fade" id="training-seminar" role="tabpanel" aria-labelledby="training-seminar-tab"><TrainingSeminar /></div>
 	    <div className="tab-pane fade" id="licensure-exam" role="tabpanel" aria-labelledby="licensure-exam-tab"><LicensureExam /></div>
