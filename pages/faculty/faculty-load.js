b7055fb0e8cd4dbc8933efd7e57bd473834f7c1d
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-12-01 17:43:14 +0800

    Adjustments to accordion menu
    Changes were made to avoid conflicts with other collapsible elements.

--- pages/faculty/faculty-load.js
+++ pages/faculty/faculty-load.js
@@ -9,8 +9,8 @@ function FacultyLoad() {
 		<h3 align = "center"> Faculty Load </h3>
 		<br />
             <div className="list-group">
-                <a className = "list-group-item list-group-item-action list-group-item-secondary" data-toggle = "collapse" href = "#sem1ay20202021">1st Semester, AY 2020-2021</a>
-		<div id = "sem1ay20202021" className = "jumbotron collapse">
+                <a className = "list-group-item list-group-item-action list-group-item-secondary" href = "#facultyloadsem1ay20202021" data-toggle = "collapse" aria-controls = "facultyloadsem1ay20202021">1st Semester, AY 2020-2021</a>
+		<div id = "facultyloadsem1ay20202021" className = "jumbotron collapse">
 			<FacultyLoadSemester />
 		</div>
 		<a className = "list-group-item list-group-item-action list-group-item-secondary">Midterm, AY 2019-2020</a>
