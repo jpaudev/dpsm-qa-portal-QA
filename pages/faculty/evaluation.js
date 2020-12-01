c8876f4552abb9d079da5df008414b1650e86f65
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-12-01 13:45:07 +0800

    New pages added
    Faculty Load and Evaluation pages now have static content

---
+++ pages/faculty/evaluation.js
@@ -0,0 +1,26 @@
+import Layout from '../../components/layout'
+import EvaluationSemester from '../../components/faculty/evaluation/evaluation-semester'
+import Link from 'next/link'
+
+function Evaluation() {
+    return (
+        <Layout>
+            <br />
+		<h3 align = "center"> Evaluation </h3>
+		<br />
+            <div className="list-group">
+                <a className = "list-group-item list-group-item-action list-group-item-secondary" data-toggle = "collapse" href = "#sem1ay20202021">1st Semester, AY 2020-2021</a>
+		<div id = "sem1ay20202021" className = "jumbotron collapse">
+			<EvaluationSemester />
+		</div>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">Midterm, AY 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">2nd Semester, 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">1st Semester, 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">Midterm, 2018-2019</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">2nd Semester, 2018-2019</a>
+            </div>
+        </Layout>
+    )
+  }
+  
+ export default Evaluation
---
+++ pages/faculty/faculty-load.js
@@ -0,0 +1,26 @@
+import Layout from '../../components/layout'
+import Link from 'next/link'
+import FacultyLoadSemester from '../../components/faculty/faculty-load/faculty-load-semester'
+
+function FacultyLoad() {
+    return (
+        <Layout>
+		<br />
+		<h3 align = "center"> Faculty Load </h3>
+		<br />
+            <div className="list-group">
+                <a className = "list-group-item list-group-item-action list-group-item-secondary" data-toggle = "collapse" href = "#sem1ay20202021">1st Semester, AY 2020-2021</a>
+		<div id = "sem1ay20202021" className = "jumbotron collapse">
+			<FacultyLoadSemester />
+		</div>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">Midterm, AY 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">2nd Semester, 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">1st Semester, 2019-2020</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">Midterm, 2018-2019</a>
+		<a className = "list-group-item list-group-item-action list-group-item-secondary">2nd Semester, 2018-2019</a>
+            </div>
+        </Layout>
+    )
+  }
+  
+export default FacultyLoad
