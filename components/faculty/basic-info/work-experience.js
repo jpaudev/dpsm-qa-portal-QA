748d1b3d767287767cd09b40e2432c61db34fcc6
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2020-12-03 12:37:02 +0800

    "Within UPM" table separated
    The "Within UPM" table is now its own component. It calls the old employment-history component (which has been unused as of late) to generate the content.

--- components/faculty/basic-info/work-experience.js
+++ components/faculty/basic-info/work-experience.js
@@ -1,38 +1,12 @@
 import Link from 'next/link'
 import WorkExpForm from './work-exp-form'
+import EmploymentHistory from './employment-history'
 
 function WorkExperience(){
 	return(
 	<div>
 			<h5 align = "center"> Within UP Manila </h5>
-			<table className = "table table-striped table-sm">
-		<tbody>
-			<tr>
-				<th>Position</th>
-				<th>Type</th>
-				<th>Start Date</th>
-				<th>End Date</th>
-			</tr>
-			<tr>
-				<td>Professor 2</td>
-				<td>Full-time (Permanent)</td>
-				<td>2020-01-26</td>
-				<td>Present</td>
-			</tr>
-			<tr>
-				<td>Professor 1</td>
-				<td>Full-time (Temporary)</td>
-				<td>2017-05-26</td>
-				<td>2020-01-25</td>
-			</tr>
-			<tr>
-				<td>Instructor 1</td>
-				<td>Part-time</td>
-				<td>2010-09-06</td>
-				<td>2017-05-25</td>
-			</tr>
-		</tbody>
-	</table>
+			<EmploymentHistory />
 			<br />
 			<h5 align = "center"> Outside UP Manila </h5>
 			<table className = "table table-striped table-sm">
