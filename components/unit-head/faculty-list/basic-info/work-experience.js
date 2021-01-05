b1ca51369c49d06ba5e28cab1f14a625cb379f39
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-01-05 13:17:19 +0800

    Added Approve and Reject functionalities

--- components/unit-head/faculty-list/basic-info/work-experience.js
+++ components/unit-head/faculty-list/basic-info/work-experience.js
@@ -5,36 +5,6 @@ function WorkExperience(){
 	<div>
 			<h5 align = "center"> Within UP Manila </h5>
 			<table className = "table table-striped table-sm">
-		<tbody>
-			<tr>
-				<th>Position</th>
-				<th>Type</th>
-				<th>Start Date</th>
-				<th>End Date</th>
-			</tr>
-			<tr>
-				<td>Professor 2</td>
-				<td>Full-time (Permanent)</td>
-				<td>2020-01-26</td>
-				<td>Present</td>
-			</tr>
-			<tr>
-				<td>Professor 1</td>
-				<td>Full-time (Temporary)</td>
-				<td>2017-05-26</td>
-				<td>2020-01-25</td>
-			</tr>
-			<tr>
-				<td>Instructor 1</td>
-				<td>Part-time</td>
-				<td>2010-09-06</td>
-				<td>2017-05-25</td>
-			</tr>
-		</tbody>
-	</table>
-			<br />
-			<h5 align = "center"> Outside UP Manila </h5>
-			<table className = "table table-striped table-sm">
 			<tbody>
 				<tr>
 					<th>Employer</th>
@@ -42,30 +12,105 @@ function WorkExperience(){
 					<th>Start Date</th>
 					<th>End Date</th>
 					<th>Description</th>
+					<th>Proof</th>
+                            		<th>Status</th>
+			    		<th>Action</th>
 				</tr>
 				<tr>
+					<td>ON Semiconductor</td>
 					<td>Systems Analyst</td>
-					<td></td>
 					<td>2020-01-26</td>
 					<td>Present</td>
-					<td></td>
+					<td>Developer of Genesis program</td>
+					<td><a href = "#">Download proof</a></td>
+			    		<td>Pending Approval</td>
+                            		<td>
+						<div className = "btn-group">
+							<a className="btn btn-info" data-toggle="modal" data-target="#approveWorkExperience">Approve</a>
+							<a className="btn btn-danger" data-toggle="modal" data-target="#rejectWorkExperience">Reject</a>
+						</div>
+			    		</td>
 				</tr>
 				<tr>
+					<td>NASA</td>
 					<td>Aeronautics Engineer</td>
-					<td></td>
 					<td>2017-05-26</td>
 					<td>2020-01-25</td>
-					<td></td>
+					<td>Principal contributor in Jet Propulsion Lab</td>
+					<td><a href = "#">Download proof</a></td>
+					<td>Pending Approval</td>
+                            		<td>
+						<div className = "btn-group">
+							<a className="btn btn-info" data-toggle="modal" data-target="#approveWorkExperience">Approve</a>
+							<a className="btn btn-danger" data-toggle="modal" data-target="#rejectWorkExperience">Reject</a>
+						</div>
+			    		</td>
 				</tr>
 				<tr>
-					<td>Interior Designer</td>
-					<td></td>
+					<td>Ubisoft</td>
+					<td>Creative Director</td>
 					<td>2010-09-06</td>
 					<td>2017-05-25</td>
-					<td></td>
+					<td>Lead in creative development of games</td>
+					<td><a href = "#">Download proof</a></td>
+					<td>Pending Approval</td>
+                            		<td>
+						<div className = "btn-group">
+							<a className="btn btn-info" data-toggle="modal" data-target="#approveWorkExperience">Approve</a>
+							<a className="btn btn-danger" data-toggle="modal" data-target="#rejectWorkExperience">Reject</a>
+						</div>
+			    		</td>
 				</tr>
 			</tbody>
 		</table>
+
+		<div className="modal fade" id="approveWorkExperience" tabIndex="-1" role="dialog" aria-labelledby="approveWorkExperienceLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="approveWorkExperienceLabel">Approve Work Experience Information</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p> Are you sure you want to approve this submission? </p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" className="btn btn-primary">Yes, approve</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="modal fade" id="rejectWorkExperience" tabIndex="-1" role="dialog" aria-labelledby="rejectWorkExperienceLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="rejectWorkExperienceLabel">Reject WorkExperience Information</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <hr />
+                        <p> Are you sure you want to reject this submission? </p>
+			<form>
+				<div className = "form-group">
+					<label htmlFor = "rejectReasonWorkExperience"> Reason </label>
+					<textarea className = "form-control" name = "rejectReasonWorkExperience" rows = "5" /> 
+				</div>
+			</form>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">No, don't reject</button>
+			<button type="button" className="btn btn-danger">Yes, reject</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
 
 </div>
 	)
