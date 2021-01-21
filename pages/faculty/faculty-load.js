ae1ea7bdedcb4712200ca9ce02faee9d3338b224
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-01-21 10:11:08 +0800

    Fixed typo in attribute

--- pages/faculty/faculty-load.js
+++ pages/faculty/faculty-load.js
@@ -11,7 +11,7 @@ function FacultyLoad() {
             <div className="list-group">
 		<a className = "list-group-item list-group-item-action list-group-item-secondary" href = "#evaluation_ay20202021" data-toggle = "collapse" aria-controls = "evaluation_ay20202021">AY 2020-2021</a>
             	<div className="list-group collapse" id = "evaluation_ay20202021">
-                	<a className = "list-group-item list-group-item-action list-group-item-secondary" href = "#facultyloadsem1ay20202021" data-toggle = "collapse" aria-controls = "facultyloadsem1ay20202021">1st Semester, AY 2020-2021</a>
+                	<a className = "list-group-item list-group-item-action list-group-item-info" href = "#facultyloadsem1ay20202021" data-toggle = "collapse" aria-controls = "facultyloadsem1ay20202021">1st Semester, AY 2020-2021</a>
 			<div id = "facultyloadsem1ay20202021" className = "jumbotron collapse">
 				<FacultyLoadSemester />
 			</div>
