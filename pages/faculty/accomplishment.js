b3b42dffeba612065d7d5842856b6250a045c62a
ajimaglanque <ssmaglanque@up.edu.ph>
2021-03-23 16:42:06 +0800

    Pages updated

--- pages/faculty/accomplishment.js
+++ pages/faculty/accomplishment.js
@@ -4,13 +4,13 @@ import PublicServiceAccomplishment from '../../components/faculty/accomplishment
 import LicensureExam from '../../components/faculty/accomplishments/licensure-exam'
 import TrainingSeminar from '../../components/faculty/accomplishments/training-seminar'
 import ResearchGrant from '../../components/faculty/accomplishments/research-grant'
+import jwt from 'jsonwebtoken'
 
 function Accomplishments(props) {
     return (
         <Layout>
             <nav>
             <div className="nav nav-tabs nav-fill nav-justified" id="nav-tab" role="tablist">
-                {/* <a className="nav-item nav-link active" id="academic-accomplishment-tab" data-toggle="tab" href="#academic-accomplishment" role="tab" aria-controls="academic-accomplishment" aria-selected="true">Academic Accomplishments</a> */}
 		<a className="nav-item nav-link active" id="public-service-accomplishment-tab" data-toggle="tab" href="#public-service-accomplishment" role="tab" aria-controls="public-service-accomplishment" aria-selected="true">Public Service Accomplishments</a>
 		<a className="nav-item nav-link" id="publication-tab" data-toggle="tab" href="#publication" role="tab" aria-controls="publication" aria-selected="false">Publications</a>
 		<a className="nav-item nav-link" id="training-seminar-tab" data-toggle="tab" href="#training-seminar" role="tab" aria-controls="training-seminar" aria-selected="false">Training/Seminars</a>
@@ -21,8 +21,7 @@ function Accomplishments(props) {
 		<br />
 		<br />
             <div className="tab-content" id="nav-tabContent">
-            {/* <div className="tab-pane fade show active" id="academic-accomplishment" role="tabpanel" aria-labelledby="academic-accomplishment-tab"><AcademicAccomplishment /></div> */}
-	    <div className="tab-pane fade show active" id="public-service-accomplishment" role="tabpanel" aria-labelledby="public-service-accomplishment-tab"><PublicServiceAccomplishment /></div>
+	    <div className="tab-pane fade show active" id="public-service-accomplishment" role="tabpanel" aria-labelledby="public-service-accomplishment-tab"><PublicServiceAccomplishment>{ props.publicService }</PublicServiceAccomplishment></div>
 	    <div className="tab-pane fade" id="publication" role="tabpanel" aria-labelledby="publication-tab"><Publication>{ props.publications }</Publication></div>
 	    <div className="tab-pane fade" id="training-seminar" role="tabpanel" aria-labelledby="training-seminar-tab"><TrainingSeminar>{ props.trainingSeminar }</TrainingSeminar></div>
 	    <div className="tab-pane fade" id="licensure-exam" role="tabpanel" aria-labelledby="licensure-exam-tab"><LicensureExam>{ props.licensureExam }</LicensureExam></div>
@@ -44,10 +43,10 @@ function Accomplishments(props) {
   }
 
 Accomplishments.getInitialProps = async () => {
-	let url = 'https://sp-api-test.alun.app/api/';
-    const res = await fetch(url + 'token',
+    let url = 'http://localhost:3001/api/'
+    const res = await fetch(url + 'login',
     {
-        body: JSON.stringify({"username": "username", "password": "password"}),
+        body: JSON.stringify({"upemail": "jpcristobal1@upm.edu.ph", "password": "password"}),
         headers: {
             'Content-Type': 'application/json'
         },
@@ -55,28 +54,45 @@ Accomplishments.getInitialProps = async () => {
     })
     
     const access = await res.json()
-    let token = access.result
-    url = 'https://sp-api-test.alun.app/api/faculty/accomplishment/';
+    let token = access.result.token
+    let facultyId = 0;
+    if (token) {
+        const json = jwt.decode(token)
+        facultyId = json.facultyId
+    } else {
+        console.log(access.result.message)
+    }
+    url = 'http://localhost:3001/api/faculty/accomplishment/' + facultyId;
     let header = {
         headers: {
             'Authorization': 'Bearer ' + token
         }
     }
 
-    const pub = await fetch(url + '9/publication', header)
+    const personal = await fetch(url, header)
+    const personalInfo = await personal.json()
+
+    const psa = await fetch(url + '/public-service', header)
+    const publicService = await psa.json()
+
+    console.log(publicService)
+
+    const pub = await fetch(url + '/publication', header)
     const publications = await pub.json()
 
-    const ts = await fetch(url + '9/training-seminar', header)
+    const ts = await fetch(url + '/training-seminar', header)
     const trainingSeminar = await ts.json()
 
-	const le = await fetch(url + '9/licensure-exam', header)
+	const le = await fetch(url + '/licensure-exam', header)
     const licensureExam = await le.json()
 
-	const rg = await fetch(url + '9/research-grant', header)
-    const researchGrant = await rg.json()    
+	const rg = await fetch(url + '/research-grant', header)
+    const researchGrant = await rg.json()
+
+    // publications.result.push(personalInfo.result)
 
     return {
-        token: token,
+        publicService: publicService.result,
     	publications: publications.result,
     	trainingSeminar: trainingSeminar.result,
     	licensureExam: licensureExam.result,
@@ -84,4 +100,4 @@ Accomplishments.getInitialProps = async () => {
     }
 }
   
-  export default Accomplishments
+export default Accomplishments
--- pages/faculty/basic-info.js
+++ pages/faculty/basic-info.js
@@ -1,4 +1,3 @@
-// import Fetch from 'isomorphic-unfetch'
 import Layout from '../../components/layout'
 import PersonalInfo from '../../components/faculty/basic-info/personal-info'
 import Education from '../../components/faculty/basic-info/education'
@@ -12,7 +11,6 @@ function BasicInfo(props) {
             <nav>
             <div className="nav nav-tabs nav-fill nav-justified" id="nav-tab" role="tablist">
                 <a className="nav-item nav-link active" id="personal-info-tab" data-toggle="tab" href="#personal-info" role="tab" aria-controls="personal-info" aria-selected="true">Personal Information</a>
-                {/* <a className="nav-item nav-link" id="emp-history-tab" data-toggle="tab" href="#emp-history" role="tab" aria-controls="emp-history" aria-selected="false">Employment History</a> */}
                 <a className="nav-item nav-link" id="educ-tab" data-toggle="tab" href="#educ" role="tab" aria-controls="educ" aria-selected="false">Education</a>
                 <a className="nav-item nav-link" id="work-exp-tab" data-toggle="tab" href="#work-exp" role="tab" aria-controls="work-exp" aria-selected="false">Work Experience</a>
             </div>
@@ -23,7 +21,6 @@ function BasicInfo(props) {
             <div className="tab-pane fade show active" id="personal-info" role="tabpanel" aria-labelledby="personal-info-tab">
                 <PersonalInfo>{ props.personalInfo }</PersonalInfo>
             </div>
-            {/* <div className="tab-pane fade" id="emp-history" role="tabpanel" aria-labelledby="emp-history-tab"><EmploymentHistory /></div> */}
             <div className="tab-pane fade" id="educ" role="tabpanel" aria-labelledby="educ-tab">
                 <Education>{ props.education }</Education>
             </div>
@@ -47,7 +44,6 @@ function BasicInfo(props) {
   }
 
 BasicInfo.getInitialProps = async () => {
-    // let url = 'https://sp-api-test.alun.app/api/';
     let url = 'http://localhost:3001/api/'
     const res = await fetch(url + 'login',
     {
@@ -67,7 +63,6 @@ BasicInfo.getInitialProps = async () => {
     } else {
         console.log(access.result.message)
     }
-    // url = 'https://sp-api-test.alun.app/api/faculty/basic-info/' + facultyId;
     url = 'http://localhost:3001/api/faculty/basic-info/' + facultyId;
     let header = {
         headers: {
@@ -98,20 +93,4 @@ BasicInfo.getInitialProps = async () => {
     }
 }
 
-//   BasicInfo.getInitialProps = async () => {
-//       let url = 'https://sp-api-test.alun.app/api/faculty/9'
-//       let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNjA1MzM5MjI4LCJleHAiOjE2MDUzNDI4Mjh9.deoiQ1EyQW0LDkKAY_T-lgjiehjeZ2KnsEGlkD2oP4A'
-//       const response = await fetch(url, {
-//           headers: {
-//               'Authorization': 'Bearer ' + token
-//           }
-//       })
-//       const personalInfo = await response.json()
-
-//       return { 
-//           personalInfo: personalInfo.result
-//         }
-//   }
-  
-
-  export default BasicInfo
+export default BasicInfo
