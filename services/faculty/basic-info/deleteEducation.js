ff006c4a6ee01b404befcf761f57daf89faa3b1f
ajimaglanque <ssmaglanque@up.edu.ph>
2021-04-07 19:51:18 +0800

    Updated Delete Education API integration

--- services/faculty/basic-info/deleteEducation.js
+++ services/faculty/basic-info/deleteEducation.js
@@ -3,16 +3,16 @@ import axios from "axios"
 export default async function deleteEducation(data) {
 	try {
 		let token = null
-		const tokenRes = await axios.post("https://sp-api-test.alun.app/api/login", {
-			upemail: "username",
+		const tokenRes = await axios.post("http://localhost:3001/api/login", {
+			upemail: "jpcristobal1@upm.edu.ph",
 			password: "password"
 		})
-
+		
 		if(tokenRes.data.success) {
-			token = tokenRes.data.result
+			token = tokenRes.data.result.token
 			try {
 				console.log(data)
-				const response = await axios.delete("https://sp-api-test.alun.app/api/faculty/basic-info/9/education", {
+				const response = await axios.delete("http://localhost:3001/api/faculty/basic-info/1/education", {
 					headers: {
 						Authorization: `Bearer ${token}`
 					},
