857385651e0a95755c94fc7c5ccd17cdc1c5b0c3
ajimaglanque <ssmaglanque@up.edu.ph>
2021-04-20 10:47:01 +0800

    API integration: POST (Basic Info > Work Experience)

--- services/faculty/basic-info/addWorkExp.js
+++ services/faculty/basic-info/addWorkExp.js
@@ -1,19 +1,15 @@
 import axios from "axios"
+import jwt from 'jsonwebtoken'
 
-export default async function addWorkExp(data) {
+export default async function addWorkExp(data, token) {
+	let cookieData = jwt.decode(token)
+    let facultyId = cookieData.facultyId
 	try {
-		let token = null
-		const tokenRes = await axios.post("http://localhost:3001/api/login", {
-			upemail: "jpcristobal1@upm.edu.ph",
-			password: "password"
-		})
-
-		if(tokenRes.data.success) {
-			token = tokenRes.data.result.token
+		if (token) {
 			let bod = null
 			if(`${data.endDate}` == "") {
 				bod = {
-					facultyId: "1",
+					facultyId: `${facultyId}`,
 					employerName: `${data.employerName}`,
 					position: `${data.position}`,
 					startDate: `${data.startDate}`,
@@ -21,7 +17,7 @@ export default async function addWorkExp(data) {
 				}
 			} else {
 				bod = {
-					facultyId: "1",
+					facultyId: `${facultyId}`,
 					employerName: `${data.employerName}`,
 					position: `${data.position}`,
 					startDate: `${data.startDate}`,
@@ -29,7 +25,6 @@ export default async function addWorkExp(data) {
 					description: `${data.description}`
 				}
 			}
-			console.log(bod)
 			try {
 				const response = await axios.post("http://localhost:3001/api/faculty/basic-info/add/work-exp", bod, {
 					headers: {
