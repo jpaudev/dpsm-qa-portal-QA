84f3676651d47899493a24b5391d4e1537eac57d
ajimaglanque <ssmaglanque@up.edu.ph>
2021-05-13 11:06:47 +0800

    Merge branch 'front-end' into fe-integ

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,6 @@ import Sidebar from './sidebar'
 import Footer from './footer'
 import Content from './content'
 import jwt from 'jsonwebtoken'
-
 function Layout(props) {
     return (
         <div className = "container-fluid">
@@ -12,6 +11,7 @@ function Layout(props) {
                 <title>DPSM-QA-PORTAL</title>
                 {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link> */}
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -24,10 +24,7 @@ function Layout(props) {
                         { props.children }
                     </div>
                 </div>
-	    </div>
-            {/* <Footer /> */}
-		
-		
+	       </div>
         </div>
     )
 }
