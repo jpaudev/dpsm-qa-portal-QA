cba0818b9afbb1280fc2ce54353708433687488f
ajimaglanque <ssmaglanque@up.edu.ph>
2021-05-25 14:10:38 +0800

    API integration: DELETE (Accomplishment > Research Grant)

---
+++ services/faculty/accomplishments/deleteResearch.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+import jwt from 'jsonwebtoken'
+
+export default async function deleteResearch(data, token) {
+	let cookieData = jwt.decode(token)
+    let facultyId = cookieData.facultyId
+	try {
+		if(token) {
+			try {
+				const response = await axios.delete("http://localhost:3001/api/faculty/accomplishment/" + facultyId + "/researcher", {
+					headers: {
+						Authorization: `Bearer ${token}`
+					},
+					data: {
+						researchId: `${data}`
+					}
+				})
+				return response.data
+			}  catch (err) {
+				console.error(err)
+				return err
+			}
+		}
+	} catch (err) {
+		console.error(err)
+		return err
+	}
+}
