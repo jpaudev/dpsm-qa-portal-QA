0e6b975200ccf2665d9a474868caee0b470549a5
jpcristobal <jpcristobal@unionbankph.com>
2021-05-31 13:01:34 +0800

    Dashboard integration for employment table

--- pages/faculty/index.js
+++ pages/faculty/index.js
@@ -2,6 +2,10 @@ import Layout from '../../components/layout'
 import Router from 'next/router'
 import jwt from 'jsonwebtoken'
 import { parseCookies } from "../../helpers"
+import AccomplishmentCount from '../../components/unit-head/dashboard/accomplishment-count/accomplishment-count'
+import EmploymentStatus from '../../components/unit-head/dashboard/employment-status/employment-status'
+import SETResults from '../../components/unit-head/dashboard/SET-results/SET-results'
+import DegreeCount from '../../components/unit-head/dashboard/degree/degree'
 
 function Dashboard(props) { 
 	if(props.data.role == 1) {
@@ -9,7 +13,6 @@ function Dashboard(props) {
 	} else if(props.data.role == 2 || props.data.role == 3){
 		return (
 	        <Layout userId={props.data.userId} facultyId={props.data.facultyId} role={props.data.role} name={props.data.name} approvalList={props.approvalList}>
-	        	{/*props*/}
 	            <div className="col-9">
 	                <div className="container">
 	                    <nav>
@@ -21,10 +24,10 @@ function Dashboard(props) {
             			</div>
             		</nav>
 	    		<div className="tab-content" id="nav-tabContent">
-	    			<div className="tab-pane fade show active" id="accomplishment-count" role="tabpanel" aria-labelledby="accomplishment-count-tab"><AccomplishmentCount /></div>
-				<div className="tab-pane fade" id="employment-status" role="tabpanel" aria-labelledby="employment-status-tab">{/*<EmploymentStatus />*/}</div>
-	    			<div className="tab-pane fade" id="SET-score" role="tabpanel" aria-labelledby="SET-score-tab">{/*<SETResults />*/}</div>
-				<div className="tab-pane fade" id="degree" role="tabpanel" aria-labelledby="degree-tab">{/*<DegreeCount />*/}</div>
+	    			<div className="tab-pane fade show active" id="accomplishment-count" role="tabpanel" aria-labelledby="accomplishment-count-tab"><AccomplishmentCount>{props.accompList}</AccomplishmentCount></div>
+				<div className="tab-pane fade" id="employment-status" role="tabpanel" aria-labelledby="employment-status-tab"><EmploymentStatus>{props.empList}</EmploymentStatus></div>
+	    			<div className="tab-pane fade" id="SET-score" role="tabpanel" aria-labelledby="SET-score-tab"><SETResults /></div>
+				<div className="tab-pane fade" id="degree" role="tabpanel" aria-labelledby="degree-tab"><DegreeCount /></div>
             		</div>
                 
 			<style jsx>{`
@@ -50,6 +53,8 @@ export async function getServerSideProps(context) {
     let data
     let personalInfo
     let approvalList
+    let accompList
+    let empList
 
     if (context.res) {
         if (Object.keys(token).length === 0 && token.constructor === Object) {
@@ -74,14 +79,25 @@ export async function getServerSideProps(context) {
             personalInfo = await personal.json()
 
             let approvalURL = 'http://localhost:3001/api/faculty/approval/' + facultyId
+            let accompURL = 'http://localhost:3001/api/faculty/reports/accomplishment'
+            let empURL = 'http://localhost:3001/api/faculty/reports/employment'
             if(data.role == 2 || data.role == 3) {
                 if(data.role == 2) {
                     approvalURL += '?unitId=' + data.unitId
+                    accompURL += '?unitId=' + data.unitId
                 }
 
                 const approval = await fetch(approvalURL, header)
                 approvalList = await approval.json()
                 approvalList = approvalList.result
+
+                const accompReports = await fetch(accompURL, header)
+                accompList = await accompReports.json()
+                accompList = accompList.result
+
+                const empReports = await fetch(empURL, header)
+                empList = await empReports.json()
+                empList = empList.result
             } else if(data.role == 1) { 
                 return {
                     redirect: {
@@ -98,7 +114,9 @@ export async function getServerSideProps(context) {
             token: token && token,
             data,
             personalInfo: personalInfo.result,
-            approvalList: approvalList
+            approvalList: approvalList,
+            accompList,
+            empList
         }
 	}
 }
