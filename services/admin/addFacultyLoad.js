357f42ea70f5d191592d6560d62df593d6f1fa0a
ajimaglanque <ssmaglanque@up.edu.ph>
2021-06-28 19:40:54 +0800

    Clerk View > Add Faculty Load

---
+++ services/admin/addFacultyLoad.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+import jwt from 'jsonwebtoken'
+
+export default async function addFacultyLoad(formData, token) {
+	let cookieData = jwt.decode(token)
+    let facultyId = cookieData.facultyId
+	try {
+		if (token) {
+			try {
+				for(var value of formData.values()) {
+					console.log(value)
+				}
+				const response = await axios({
+				    method: 'POST',
+				    url: 'http://localhost:3001/api/faculty/load',
+				    data: formData,
+				    headers: {'Content-Type': 'multipart/form-data', Authorization: `Bearer ${token}`}
+			    })	
+				return response.data
+			} catch (err) {
+				console.error(err)
+				return err
+			}
+		}
+	} catch (err) {
+		console.error(err)
+		return err
+	}
+}
