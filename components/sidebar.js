9e8e3acba009ccb84a41411c49049c420ad67ff7
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-08-05 08:17:05 +0800

    Added highlight for selected menu item
    Uses query parameter and checks route pathname to track which menu item to highlight

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,7 +1,13 @@
 import Link from 'next/link'
 import { useCookies } from "react-cookie";
+import { useRouter } from 'next/router'
 
 function Sidebar(props) { 
+    const router = useRouter()
+    const {
+    	query: { menu },
+    } = router
+
     const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 
     function handleRemoveCookie() {
@@ -23,25 +29,25 @@ function Sidebar(props) {
                 <div className="list-group">
 			<a className = "list-group-item list-group-item-action list-group-item-secondary" data-toggle = "collapse" href = "#facultyMenu" aria-controls = "facultyMenu"> Faculty </a>
                 <div id = "facultyMenu" className = "collapse show">
-                    { !staff && !faculty && <Link href="/faculty"><a className = "list-group-item list-group-item-action list-group-item-success">Dashboard</a></Link> }
-                    { !staff && <Link href="/faculty/basic-info"><a className = "list-group-item list-group-item-action list-group-item-success">Basic Information</a></Link> }
-                    { !staff && <Link href="/faculty/accomplishment"><a className = "list-group-item list-group-item-action list-group-item-success">Accomplishment</a></Link> }
-                    { !staff && <Link href="/faculty/faculty-load"><a className = "list-group-item list-group-item-action list-group-item-success">Faculty Load</a></Link> }
-                    { !staff && <Link href="/faculty/evaluation"><a className = "list-group-item list-group-item-action list-group-item-success ">Peer Evaluation</a></Link> }
-                    { !staff && !faculty && <Link href="/faculty/view/all"><a className = "list-group-item list-group-item-action list-group-item-success ">Faculty List</a></Link> }
-                    { !staff && !faculty && <Link href="/faculty/approval">
-                        <a className = "list-group-item list-group-item-action list-group-item-success "> 
+                    { !staff && !faculty && <Link href={{ pathname: "/faculty", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty" ?  menu  : "inactive" }>Dashboard</a></Link> }
+                    { !staff && <Link href={{ pathname: "/faculty/basic-info", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty/basic-info" ?  menu  : "inactive" }>Basic Information</a></Link> }
+                    { !staff && <Link href={{ pathname: "/faculty/accomplishment", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty/accomplishment" ?  menu  : "inactive" }>Accomplishment</a></Link> }
+                    { !staff && <Link href={{ pathname: "/faculty/faculty-load", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty/faculty-load" ?  menu  : "inactive" }>Faculty Load</a></Link> }
+                    { !staff && <Link href={{ pathname: "/faculty/evaluation", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success " id = { router.pathname === "/faculty/evaluation" ?  menu  : "inactive" }>Peer Evaluation</a></Link> }
+                    { !staff && !faculty && <Link href={{ pathname: "/faculty/view/all", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success " id = { router.pathname === "/faculty/view/all" ?  menu  : "inactive" }>Faculty List</a></Link> }
+                    { !staff && !faculty && <Link href={{ pathname: "/faculty/approval", query: { menu: "active" } }}>
+                        <a className = "list-group-item list-group-item-action list-group-item-success " id = { router.pathname === "/faculty/approval" ?  menu  : "inactive" }> 
                             Pending Approvals &nbsp;{props.approvalList.facultyCount > 0 && <span className="badge badge-danger">{props.approvalList.facultyCount}</span>}
                         </a>
                     </Link> }
-                    { staff && <Link href="/admin"><a className = "list-group-item list-group-item-action list-group-item-success ">Faculty List</a></Link> }
-		            { !staff && !faculty && props.roleAssignment && <Link href ="/faculty/role-assignment">
-                        <a className = "list-group-item list-group-item-action list-group-item-success">
+                    { staff && <Link href={{ pathname: "/admin", query: { menu: "active" } }}><a className = "list-group-item list-group-item-action list-group-item-success " id = { router.pathname === "/admin" ?  menu  : "inactive" }>Faculty List</a></Link> }
+		            { !staff && !faculty && props.roleAssignment && <Link href ={{ pathname: "/faculty/role-assignment", query: { menu: "active" } }}>
+                        <a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty/role-assignment" ?  menu  : "inactive" }>
                             Role Assignment &nbsp;<span className="badge badge-danger">!</span>
                         </a>
                     </Link> }
-                    { !staff && !faculty && !props.roleAssignment && <Link href ="/faculty/role-assignment">
-                        <a className = "list-group-item list-group-item-action list-group-item-success">
+                    { !staff && !faculty && !props.roleAssignment && <Link href ={{ pathname: "/faculty/role-assignment", query: { menu: "active" } }}>
+                        <a className = "list-group-item list-group-item-action list-group-item-success" id = { router.pathname === "/faculty/role-assignment" ?  menu  : "inactive" }>
                             Role Assignment
                         </a>
                     </Link> }
@@ -66,6 +72,10 @@ function Sidebar(props) {
 			}
 			.list-group-item-success{
 				text-indent:20%;
+			}
+			#active{
+				background-color: #005919;
+				color: #fff;
 			}
 		`}</style>
             </div>           
