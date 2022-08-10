1cc38a8e2436e925e24b0fb697875fd019f4819a
Adrian Santos <apsantos8@up.edu.ph>
2022-08-10 13:54:12 +0800

    Cleaned layout.js

--- components/layout.js
+++ components/layout.js
@@ -13,43 +13,12 @@ function Layout(props) {
     let approvalList
     if(props.approvalList) approvalList = props.approvalList
     return (
-        // Old Code
-        // <div className = "container-fluid">
-        //     <Head>
-        //         <title>DPSM-QA-PORTAL MOD</title>
-        //         {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link> */}
-        //         <link rel="stylesheet" href= "/testStyle.css" />
-        //         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-        //         <link rel="stylesheet" hrsef="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
-        //         {/* MATERIAL CDN */}
-        //         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet"/>
-        //         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        //         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        //         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        //     </Head>
-        //     <Header name={ props.name } role={props.role}/>
-        //     <div className = "row" id = "layout_row">
-        //         <Sidebar role = {props.role} approvalList={approvalList} facultyId = {props.facultyId} roleAssignment={props.roleAssignmentFlag} />
-        //             <div className="col" id = "layout_content">
-        //                 { props.children }
-        //             </div>
-	    //    </div>
-        // </div>
-
-
-
-
         <div>
             <Head>
                 <title id="demo1">DPSM QA Portal</title>
-                
-
-                {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link> */}
-                {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" /> */}
+                {/* Stylesheet */}
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-
                 <link rel="stylesheet" href= "/testStyle.css" />
-
                 {/* Fonts and Iconds */}
                 <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <link rel="stylesheet" hrsef="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
@@ -64,87 +33,85 @@ function Layout(props) {
                 <script src="/testScript.js"> </script>
 
             </Head>
+            
             {/* Header -- Topbar */}
             <Header name={ props.name } role={props.role}/>
 
             <body>
 
-            {/* Frame While CSS is not fully loaded */}
-            <div id="loadOverlay" style={{backgroundColor: "#333", position: "absolute", top: "0px", left: "0px", width: "100%" , height: "100%", zIndex: "2000"}}></div>
-            
-                {/* Main Container */}
-                <div className="containerMain">
-                    {/* Sidebar */}
-                    <Sidebar role = {props.role} approvalList={approvalList} facultyId = {props.facultyId} roleAssignment={props.roleAssignmentFlag} />
+                {/* Frame While CSS is not fully loaded */}
+                <div id="loadOverlay" style={{backgroundColor: "#333", position: "absolute", top: "0px", left: "0px", width: "100%" , height: "100%", zIndex: "2000"}}></div>
+                
+                    {/* Main Container */}
+                    <div className="containerMain">
+                        {/* Sidebar */}
+                        <Sidebar role = {props.role} approvalList={approvalList} facultyId = {props.facultyId} roleAssignment={props.roleAssignmentFlag} />
 
-                    {/* Content */}
-                    <main>
-                        <div className="theme-toggler tablet-only" onClick={() => {
-                                        const themeToggler = document.querySelector(".theme-toggler")
-                                        document.body.classList.toggle('dark-theme-variables')
-                                        themeToggler.querySelector('span').classList.toggle('active')
-                                    }}>
-                                <span className="material-icons-sharp active">light_mode</span>
-                                <span className="material-icons-sharp">dark_mode</span>
-                        </div>
-                        <br></br>
-                        {props.children}
-                    </main>
-
-                    {/* right */}
-                    <div className="right">
-
-                        {/* TOP */}
-                        <div className="top" >
-                            <button id="menu-btn" style={{color: "white"}} onClick={() => {
-                                        const sideMenu = document.querySelector("aside")
-                                        sideMenu.style.display = 'block'
-
-                                    }}>
-                                <span className="material-icons-sharp">menu</span>
-                            </button>
-                            <div className="theme-toggler" onClick={() => {
-                                        const themeToggler = document.querySelector(".theme-toggler")
-                                        document.body.classList.toggle('dark-theme-variables')
-                                        themeToggler.querySelector('span').classList.toggle('active')
-                                    }}>
-                                <span className="material-icons-sharp active">light_mode</span>
-                                <span className="material-icons-sharp">dark_mode</span>
+                        {/* Content */}
+                        <main>
+                            <div className="theme-toggler tablet-only" onClick={() => {
+                                            const themeToggler = document.querySelector(".theme-toggler")
+                                            document.body.classList.toggle('dark-theme-variables')
+                                            themeToggler.querySelector('span').classList.toggle('active')
+                                        }}>
+                                    <span className="material-icons-sharp active">light_mode</span>
+                                    <span className="material-icons-sharp">dark_mode</span>
                             </div>
-                            
-                            <div className="profile">
-                                <div className="info">
-                                    <p>Logged In As: <br/> <b>{props.name}</b></p>
-                                    { props.role == "1" &&
-                                        <p className="text-muted">Faculty</p>
-                                    }
-                                    { props.role == "2" &&
-                                        <p className="text-muted">Unit Head</p>
-                                    }
-                                    { props.role == "3" &&
-                                        <p className="text-muted">Department Chair</p>
-                                    }
+                            <br></br>
+                            {props.children}
+                        </main>
 
-                                    { props.role == "4" &&
-                                        <p className="text-muted">DAPC</p>
-                                    }
-                                    { props.role == "5" &&
-                                        <p className="text-muted">Admin Staff</p>
-                                    }
+                        {/* right */}
+                        <div className="right">
+
+                            {/* TOP */}
+                            <div className="top" >
+                                <button id="menu-btn" style={{color: "white"}} onClick={() => {
+                                            const sideMenu = document.querySelector("aside")
+                                            sideMenu.style.display = 'block'
+
+                                        }}>
+                                    <span className="material-icons-sharp">menu</span>
+                                </button>
+                                <div className="theme-toggler" onClick={() => {
+                                            const themeToggler = document.querySelector(".theme-toggler")
+                                            document.body.classList.toggle('dark-theme-variables')
+                                            themeToggler.querySelector('span').classList.toggle('active')
+                                        }}>
+                                    <span className="material-icons-sharp active">light_mode</span>
+                                    <span className="material-icons-sharp">dark_mode</span>
+                                </div>
                                 
-                                </div>
-                                <div className="profile-photo">
-                                    <img src="/profile-1.jpg"/>
+                                <div className="profile">
+                                    <div className="info">
+                                        <p>Logged In As: <br/> <b>{props.name}</b></p>
+                                        { props.role == "1" &&
+                                            <p className="text-muted">Faculty</p>
+                                        }
+                                        { props.role == "2" &&
+                                            <p className="text-muted">Unit Head</p>
+                                        }
+                                        { props.role == "3" &&
+                                            <p className="text-muted">Department Chair</p>
+                                        }
+
+                                        { props.role == "4" &&
+                                            <p className="text-muted">DAPC</p>
+                                        }
+                                        { props.role == "5" &&
+                                            <p className="text-muted">Admin Staff</p>
+                                        }
+                                    
+                                    </div>
+                                    <div className="profile-photo">
+                                        <img src="/profile-1.jpg"/>
+                                    </div>
                                 </div>
                             </div>
+                            {/* End Top */}
+
                         </div>
-                        {/* End Top */}
-
-
-                    </div>
                 </div>
-
-
 
             </body>
 
