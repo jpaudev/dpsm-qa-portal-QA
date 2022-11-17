42f6c3f03c2f83c7eb5a21773d9e0207654b50b6
jpaudev <csjakpaulene@gmail.com>
2022-11-17 17:46:47 +0800

    Removed unused code in change password

--- pages/settings/change-password.js
+++ pages/settings/change-password.js
@@ -3,30 +3,16 @@ import jwt from 'jsonwebtoken'
 import ChangePasswordComponent from '../../components/change-password'
 
 function ChangePassword(props) {
-    if(props.data.role != 5) {
-        return (
-            <>
-                <ChangePasswordComponent token={props.token.user}>{props.data}</ChangePasswordComponent>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <ChangePasswordComponent token={props.token.user}>{props.data}</ChangePasswordComponent>
-            </>
-        )
-    }
+    return (
+        <>
+            <ChangePasswordComponent token={props.token.user}>{props.data}</ChangePasswordComponent>
+        </>
+    )
   }
 
   export async function getServerSideProps(context) {
     const token = parseCookies(context.req)
     let data
-    let personalInfo
-    let approvalList
-    let accompList
-    let empList
-    let educList
-    let roleAssignmentFlag = false
 
     if (context.res) {
         if (isExpired(token.user) || Object.keys(token).length === 0 && token.constructor === Object) {
@@ -38,92 +24,15 @@ function ChangePassword(props) {
             }
         } else {
             data = jwt.decode(token.user)
-        
-            if(data.role != 5) {
-                let facultyId = data.facultyId
-            
-                let header = {
-                    headers: {
-                        'Authorization': 'Bearer ' + token.user
-                    }
-                }
-                
-                const personal = await fetch(process.env.API_URL + '/faculty/basic-info/' + facultyId, header)
-                personalInfo = await personal.json()
-    
-                let approvalURL = process.env.API_URL + '/faculty/approval/' + facultyId
-                let accompURL = process.env.API_URL + '/faculty/reports/accomplishment'
-                let empURL = process.env.API_URL + '/faculty/reports/employment'
-                let educURL = process.env.API_URL + '/faculty/reports/education'
-                let roleAssignmentURL = process.env.API_URL + '/faculty/basic-info/unit/assignment'
-                
-                if(data.role == 2 || data.role == 3) {
-                    if(data.role == 2) {
-                        approvalURL += '?unitId=' + data.unitId
-                        accompURL += '?unitId=' + data.unitId
-                        empURL += '?unitId=' + data.unitId
-                        educURL += '?unitId=' + data.unitId
-                        roleAssignmentURL += '?unitId=' + data.unitId
-                    }
-    
-                    const approval = await fetch(approvalURL, header)
-                    approvalList = await approval.json()
-                    approvalList = approvalList.result
-    
-                    const accompReports = await fetch(accompURL, header)
-                    accompList = await accompReports.json()
-                    accompList = accompList.result
-    
-                    const empReports = await fetch(empURL, header)
-                    empList = await empReports.json()
-                    empList = empList.result
-    
-                    const educReports = await fetch(educURL, header)
-                    educList = await educReports.json()
-                    educList = educList.result
-    
-                    const roleAssignments = await fetch(roleAssignmentURL, header)
-                    let roleAssignmentList = await roleAssignments.json()
-                    roleAssignmentList = roleAssignmentList.result
-                    if(data.role == 2) {
-                        if(roleAssignmentList.approverRemarks != null) roleAssignmentFlag = true
-                    } else if(data.role == 3 && roleAssignmentList) {
-                        roleAssignmentFlag = true 
-                    }
-                // } else if(data.role == 1) { 
-                //     return {
-                //         redirect: {
-                //             destination: '/faculty/basic-info',
-                //             permanent: true,
-                //         },
-                //     }
-                }
-            }
         }
     } 
 
-    if(data.role != 5) {
-        return {
-            props: {
-                token: token && token,
-                data,
-                personalInfo: personalInfo.result,
-                approvalList: approvalList || null,
-                accompList: accompList || null,
-                empList: empList || null,
-                educList: educList || null,
-                roleAssignmentFlag
-            }
-        }
-    } else {
-        return {
-            props: {
-                token: token && token,
-                data
-            }
+    return {
+        props: {
+            token: token && token,
+            data
         }
     }
-
 }
 
   export default ChangePassword
