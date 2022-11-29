7b98e891591d695f6fe8ce3e6d341e93fe26fd29
jpaudev <csjakpaulene@gmail.com>
2022-11-29 14:37:50 +0800

    Changed tab icon

--- components/layout.js
+++ components/layout.js
@@ -37,6 +37,7 @@ function Layout(props) {
         <div>
             <Head>
                 <title id="demo1">UPM QA Portal</title>
+                <link rel="icon" href="/logo.png" />
                 {/* Stylesheet */}
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href= "/main.css" />
